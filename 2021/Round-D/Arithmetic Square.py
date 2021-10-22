25c28c9887618621a26219f7fe82836cb931fd19
Seungjun <60959924+seungjun-green@users.noreply.github.com>
2021-10-22 12:14:52 +0900

    Update Arithmetic Square.py

--- 2021/Round-D/Arithmetic Square.py
+++ 2021/Round-D/Arithmetic Square.py
@@ -1,9 +1,11 @@
 def countAR(R1,R2,R3, val):
+    # remaking the R2
     temp = R2[1]
     R2[1] = val
     R2.append(temp)
 
-    #R1,R2,R3,
+
+    # Generating rows to be checked
     R4 = [R1[0], R2[0], R3[0]]
     R5 = [R1[1], R2[1], R3[1]]
     R6 = [R1[2], R2[2], R3[2]]
@@ -14,6 +16,8 @@ def countAR(R1,R2,R3, val):
     count = 0
 
     dataSet = [R1,R2,R3,R4,R5,R6,R7,R8]
+    
+    # check all possible 8 rows
     for row in dataSet:
         if check(row):
             count += 1
@@ -36,6 +40,9 @@ def solution(R1,R2,R3):
 
     possible = [(R1[0]+R3[2])//2,(R1[2]+R3[0])//2,(R2[0]+R2[1])//2,(R1[1]+R3[1])//2]
 
+
+    # Iterating all possible mat[1][1], check number of Arithmetic rows,
+    # keep updating global_max, and after iteration ended, return the global_max
     for i in possible:
         current_count = countAR(R1,R2.copy(),R3,i)
 
